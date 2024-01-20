e771999ea2a8ec3ab3f4857fdf614a84849f3800
John Doe <johndoe@example.com>
2024-01-20 18:58:05 +0200

    proxy upd

--- src/proxy/index.ts
+++ src/proxy/index.ts
@@ -1,13 +1,21 @@
 import { HttpsProxyAgent } from "https-proxy-agent";
 import { SocksProxyAgent } from "socks-proxy-agent";
+import { z } from "zod";
+import { ipOrDomainSchema } from "../zod";
 
-type ProxyItem = {
-  type: "http" | "socks";
-  host: string;
-  port: number | string;
-  username: string;
-  password: string;
-};
+const proxySchema = z.object({
+  type: z.union([z.literal("https"), z.literal("socks")]),
+  host: ipOrDomainSchema,
+  port: z
+    .string()
+    .regex(/\d+/, "Must be a number")
+    .transform((str) => Number(str)),
+  username: z.string(),
+  password: z.string(),
+  changeUrl: z.string().url().optional(),
+});
+
+export type ProxyItem = Omit<z.infer<typeof proxySchema>, "changeUrl">;
 
 export const getProxyAgent = (proxy?: ProxyItem) => {
   if (!proxy) return undefined;
@@ -15,7 +23,7 @@ export const getProxyAgent = (proxy?: ProxyItem) => {
   const { type, host, port, username, password } = proxy;
 
   switch (type) {
-    case "http": {
+    case "https": {
       return new HttpsProxyAgent(
         `http://${username}:${password}@${host}:${port}`
       );
@@ -31,30 +39,7 @@ export const getProxyAgent = (proxy?: ProxyItem) => {
   }
 };
 
-import { z } from "zod";
-
-const ipOrDomainPattern =
-  /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$|^(?:(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.?)+(?:[A-Za-z]{2,6})$/;
-
-const ipOrDomainSchema = z
-  .string()
-  .refine((value) => ipOrDomainPattern.test(value), {
-    message: "Invalid IP or domain format",
-  });
-
-const proxySchema = z.object({
-  type: z.union([z.literal("http"), z.literal("socks")]),
-  host: ipOrDomainSchema,
-  port: z
-    .string()
-    .regex(/\d+/, "Must be a number")
-    .transform((str) => Number(str)),
-  username: z.string(),
-  password: z.string(),
-  changeUrl: z.string().url().optional(),
-});
-
-export const getProxies = (proxy: string, divider = ";") => {
+export const parseProxy = (proxy: string, divider = ";") => {
   const [type, host, port, user, pass, changeUrl] = proxy.split(divider);
   return proxySchema.parse({ type, host, port, user, pass, changeUrl });
 };
